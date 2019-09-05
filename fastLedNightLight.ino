e44a97837d089847d7c915fbdc03b448e4674896
Ron Senykoff <rsenykoff@gmail.com>
2019-09-05 06:53:33 -0400

    Some code cleanup.

--- fastLedNightLight.ino
+++ fastLedNightLight.ino
@@ -82,6 +82,258 @@ DEFINE_GRADIENT_PALETTE( sky_31_gp ) {
 };
 
 
+/**** END OF NO BLUE PALETTES ************************************/
+
+
+// Gradient palette "All_kinds_of_green_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/jjg/ccolo/smorin2002/tn/All_kinds_of_green.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 44 bytes of program space.
+
+DEFINE_GRADIENT_PALETTE( All_kinds_of_green_gp ) {
+    0, 203,248, 95,
+   25, 203,248, 95,
+   51, 167,223, 87,
+   76, 135,197, 78,
+  102, 104,197, 63,
+  127,  78,197, 49,
+  153,  56,184, 51,
+  178,  39,173, 52,
+  204,  30,136, 43,
+  229,  21,105, 36,
+  255,  21,105, 36};
+
+// Gradient palette "sky_21_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/rafi/tn/sky-21.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 20 bytes of program space.
+
+DEFINE_GRADIENT_PALETTE( sky_21_gp ) {
+    0, 255,164, 49,
+   40, 227,141, 72,
+   87, 125,149,135,
+  178,   0, 31, 52,
+  255,   1, 10, 22};
+
+// Gradient palette "springangel_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/rc/tn/springangel.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 44 bytes of program space.
+
+DEFINE_GRADIENT_PALETTE( springangel_gp ) {
+    0,  95,151,140,
+   48,  35,101,170,
+   71,  74,103,111,
+   94,  77, 97,111,
+  117, 165,142,132,
+  140, 186,142,128,
+  163, 182,124, 96,
+  186, 188, 95, 57,
+  209, 220, 88, 28,
+  232,  92, 86, 41,
+  255,  92, 86, 41};
+
+// Gradient palette "Adrift_in_Dreams_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/jjg/ccolo/Skyblue2u/tn/Adrift_in_Dreams.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 44 bytes of program space.
+
+DEFINE_GRADIENT_PALETTE( Adrift_in_Dreams_gp ) {
+    0, 148,223, 77,
+   25, 148,223, 77,
+   51, 115,201, 83,
+   76,  86,182, 89,
+  102,  57,156, 80,
+  127,  36,131, 72,
+  153,  17, 93, 61,
+  178,   5, 61, 51,
+  204,   1, 34, 38,
+  229,   1, 15, 29,
+  255,   1, 15, 29};
+
+// Gradient palette "liahlah2_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/rc/tn/liahlah2.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 40 bytes of program space.
+
+DEFINE_GRADIENT_PALETTE( liahlah2_gp ) {
+    0,  38, 73,145,
+   30,   7, 19, 51,
+   56,  73, 40,230,
+   86,  55, 49,182,
+  112,  15,135,250,
+  140, 125,122,212,
+  170,  13, 55,228,
+  198, 123, 65,228,
+  229,   7, 19, 51,
+  255,  38, 73,145};
+
+// Gradient palette "springbird_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/rc/tn/springbird.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 16 bytes of program space.
+
+DEFINE_GRADIENT_PALETTE( springbird_gp ) {
+    0,  10, 45,147,
+   38,  10, 45,147,
+  216, 192,121,162,
+  255, 192,121,162};
+
+
+
+/****** COOL2 ****/
+
+// Gradient palette "spellbound_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/rc/tn/spellbound.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 84 bytes of program space.
+
+DEFINE_GRADIENT_PALETTE( spellbound_gp ) {
+    0, 232,235, 40,
+   12, 157,248, 46,
+   25, 100,246, 51,
+   45,  53,250, 33,
+   63,  18,237, 53,
+   81,  11,211,162,
+   94,  18,147,214,
+  101,  43,124,237,
+  112,  49, 75,247,
+  127,  49, 75,247,
+  140,  92,107,247,
+  150, 120,127,250,
+  163, 130,138,252,
+  173, 144,131,252,
+  186, 148,112,252,
+  196, 144, 37,176,
+  211, 113, 18, 87,
+  221, 163, 33, 53,
+  234, 255,101, 78,
+  247, 229,235, 46,
+  255, 229,235, 46};
+
+// Gradient palette "titannightfall_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/rc/tn/titannightfall.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 28 bytes of program space.
+
+DEFINE_GRADIENT_PALETTE( titannightfall_gp ) {
+    0, 173, 72,109,
+   28, 112, 33, 68,
+   51,  72, 19, 67,
+  127,   2,  1, 33,
+  204,  72, 19, 67,
+  229, 112, 33, 68,
+  255, 173, 72,109};
+
+// Gradient palette "trove_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/rc/tn/trove.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 84 bytes of program space.
+
+DEFINE_GRADIENT_PALETTE( trove_gp ) {
+    0,  12, 23, 11,
+   12,   8, 52, 27,
+   25,  32,142, 64,
+   38,  55, 68, 30,
+   51, 190,135, 45,
+   63, 201,175, 59,
+   76, 186, 80, 20,
+   89, 220, 79, 32,
+  101, 184, 33, 14,
+  114, 137, 16, 15,
+  127, 118, 20, 27,
+  140,  79, 16, 35,
+  153,  67,  8, 26,
+  165,  22,  9, 42,
+  178,  11,  3, 34,
+  191,  58, 31,109,
+  204, 186, 49, 83,
+  216, 182, 25, 55,
+  229,  39, 90,100,
+  242,  15, 81,132,
+  255,  68,135, 52};
+
+// Gradient palette "sunlitwave_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/rc/tn/sunlitwave.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 32 bytes of program space.
+
+DEFINE_GRADIENT_PALETTE( sunlitwave_gp ) {
+    0,   5,  9, 84,
+   45,  37, 24,111,
+   81,  16,  5, 59,
+  112,  24,  1, 20,
+  150,  34,  1,  2,
+  198, 175, 36,  7,
+  237, 208,104, 16,
+  255, 239,211,158};
+
+// Gradient palette "hangonfatboy_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/rc/tn/hangonfatboy.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 40 bytes of program space.
+
+DEFINE_GRADIENT_PALETTE( hangonfatboy_gp ) {
+    0, 215, 74,  6,
+   30,  74, 22, 53,
+   61,  18,  6, 27,
+   96,   5, 36, 85,
+  114,   1,  8, 30,
+  140,   1,  8, 30,
+  158,   5, 36, 85,
+  193,  18,  6, 27,
+  224,  74, 22, 53,
+  255, 215, 74,  6};
+
+// Gradient palette "serendil_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/rc/tn/serendil.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 24 bytes of program space.
+
+DEFINE_GRADIENT_PALETTE( serendil_gp ) {
+    0,   0, 55,  0,
+  117,  61, 81,  9,
+  137, 132, 86, 10,
+  191, 215,221, 42,
+  211, 237,112,184,
+  255, 192, 19, 19};
+
+// Gradient palette "mellon_ball_surprise_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/jjg/ccolo/Skyblue2u/tn/mellon_ball_surprise.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 44 bytes of program space.
+
+DEFINE_GRADIENT_PALETTE( mellon_ball_surprise_gp ) {
+    0, 152,227, 85,
+   25, 152,227, 85,
+   51, 182,235, 95,
+   76, 215,244,106,
+  102, 234,189, 79,
+  127, 255,142, 56,
+  153, 255,115, 51,
+  178, 255, 90, 45,
+  204, 242, 59, 52,
+  229, 229, 36, 62,
+  255, 229, 36, 62};
+
+// Gradient palette "Everglowing_Wonder_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/jjg/ccolo/electroluv/tn/Everglowing_Wonder.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 44 bytes of program space.
+
+DEFINE_GRADIENT_PALETTE( Everglowing_Wonder_gp ) {
+    0, 167,244, 89,
+   71, 167,244, 89,
+  142, 126,211, 52,
+  146,  92,182, 26,
+  150,  78,130, 20,
+  164,  66, 86, 14,
+  178,  55, 44, 14,
+  186,  46, 16, 13,
+  193,  40, 10, 10,
+  224,  34,  5,  9,
+  255,  34,  5,  9};
+
 
 
 
@@ -100,9 +352,6 @@ DEFINE_GRADIENT_PALETTE( sky_33_gp ) {
   255,   5, 19, 31
 };
 
-
-
-
 // Gradient palette "sky_45_gp", originally from
 // http://soliton.vm.bytemark.co.uk/pub/cpt-city/rafi/tn/sky-45.png.index.html
 // converted for FastLED with gammas (2.6, 2.2, 2.5)
@@ -116,8 +365,6 @@ DEFINE_GRADIENT_PALETTE( sky_45_gp ) {
   255,   0,  2, 23
 };
 
-
-
 // Gradient palette "aquamarinemermaid_gp", originally from
 // http://soliton.vm.bytemark.co.uk/pub/cpt-city/rc/tn/aquamarinemermaid.png.index.html
 // converted for FastLED with gammas (2.6, 2.2, 2.5)
@@ -130,27 +377,6 @@ DEFINE_GRADIENT_PALETTE( aquamarinemermaid_gp ) {
   191,   7, 55, 164,
   255,  23,  4, 32
 };
-
-
-
-// Gradient palette "liahlah2_gp", originally from
-// http://soliton.vm.bytemark.co.uk/pub/cpt-city/rc/tn/liahlah2.png.index.html
-// converted for FastLED with gammas (2.6, 2.2, 2.5)
-// Size: 40 bytes of program space.
-
-DEFINE_GRADIENT_PALETTE( liahlah2_gp ) {
-  0,  38, 73, 145,
-  30,   7, 19, 51,
-  56,  73, 40, 230,
-  86,  55, 49, 182,
-  112,  15, 135, 250,
-  140, 125, 122, 212,
-  170,  13, 55, 228,
-  198, 123, 65, 228,
-  229,   7, 19, 51,
-  255,  38, 73, 145
-};
-
 
 // Gradient palette "Quick_Vent_gp", originally from
 // http://soliton.vm.bytemark.co.uk/pub/cpt-city/lb/mp/tn/Quick_Vent.png.index.html
@@ -230,7 +456,6 @@ DEFINE_GRADIENT_PALETTE( Wild_Orange_gp ) {
   255, 255, 36,  1
 };
 
-
 // Gradient palette "Molten_lava_gp", originally from
 // http://soliton.vm.bytemark.co.uk/pub/cpt-city/lb/mp/tn/Molten_lava.png.index.html
 // converted for FastLED with gammas (2.6, 2.2, 2.5)
@@ -247,63 +472,6 @@ DEFINE_GRADIENT_PALETTE( Molten_lava_gp ) {
   255, 220, 42,  4
 };
 
-// Gradient palette "galaxy02_gp", originally from
-// http://soliton.vm.bytemark.co.uk/pub/cpt-city/pd/astro/tn/galaxy02.png.index.html
-// converted for FastLED with gammas (2.6, 2.2, 2.5)
-// Size: 196 bytes of program space.
-
-DEFINE_GRADIENT_PALETTE( galaxy02_gp ) {
-  0, 247, 133, 36,
-  5, 210, 125, 41,
-  10, 179, 118, 45,
-  15, 157, 103, 41,
-  21, 135, 90, 37,
-  26, 125, 85, 33,
-  31, 117, 81, 30,
-  37,  95, 68, 25,
-  42,  77, 56, 21,
-  47,  58, 44, 15,
-  53,  42, 33, 11,
-  58,  31, 27, 10,
-  63,  22, 22,  8,
-  69,  20, 20,  9,
-  74,  18, 18, 10,
-  79,  19, 20, 10,
-  84,  19, 22, 10,
-  90,  24, 26, 13,
-  95,  30, 31, 17,
-  100,  34, 39, 23,
-  106,  39, 47, 30,
-  111,  41, 62, 37,
-  116,  44, 79, 44,
-  122,  47, 70, 38,
-  127,  51, 60, 34,
-  132,  53, 55, 31,
-  138,  56, 49, 29,
-  143,  55, 52, 35,
-  148,  54, 54, 42,
-  154,  50, 49, 34,
-  159,  46, 45, 28,
-  164,  46, 47, 31,
-  170,  46, 49, 34,
-  175,  50, 51, 31,
-  180,  56, 52, 27,
-  185,  59, 59, 37,
-  191,  64, 68, 50,
-  196,  66, 77, 47,
-  201,  69, 85, 43,
-  207,  78, 80, 43,
-  212,  87, 77, 43,
-  217,  94, 81, 46,
-  223, 103, 86, 49,
-  228, 117, 96, 59,
-  233, 133, 107, 69,
-  239, 163, 135, 84,
-  244, 199, 164, 102,
-  249, 224, 171, 100,
-  255, 252, 176, 98
-};
-
 // Gradient palette "gr67_hult_gp", originally from
 // http://soliton.vm.bytemark.co.uk/pub/cpt-city/hult/tn/gr67_hult.png.index.html
 // converted for FastLED with gammas (2.6, 2.2, 2.5)
@@ -316,7 +484,6 @@ DEFINE_GRADIENT_PALETTE( gr67_hult_gp ) {
   191, 194, 175,  3,
   255,  42,  5,  1
 };
-
 
 // Gradient palette "Fuschia_6_gp", originally from
 // http://soliton.vm.bytemark.co.uk/pub/cpt-city/ds/fuschia/tn/Fuschia-6.png.index.html
@@ -367,7 +534,7 @@ String getButtonStatus() {
 
 // List of patterns to cycle through.  Each is defined as a separate function below.
 typedef void (*SimplePatternList[])();
-SimplePatternList gPatterns = { warmPalette1, rainbow, rainbowWithGlitter, confetti, sinelon, fillSolid, fillGradient1, fillGradient2 }; //*****************
+SimplePatternList gPatterns = { warmPalette1, coolPalette1, coolPalette2, rainbow, rainbowWithGlitter, confetti, sinelon, fillSolid, fillGradient1, fillGradient2 }; //*****************
 
 uint8_t gCurrentPatternNumber = 0; // Index number of which pattern is current
 
@@ -397,14 +564,13 @@ void setup() {
 }
 
 
-
 String buttonStatus = "";
 String newStatus = "";
 
 
-
 static uint8_t startIndex = 0;
 int pos = 0;
+
 void loop()
 {
   encoder1.tick();
@@ -417,20 +583,17 @@ void loop()
   } else if (newPos > ROTARYMAX) {
     encoder1.setPosition(ROTARYMIN / ROTARYSTEPS);
     newPos = ROTARYMIN;
-  } // if
+  } 
 
   if (pos != newPos) {
     Serial.print(newPos);
     Serial.println();
     pos = newPos;
-  } // if
+  } 
 
   // I found I needed to use this 'delay' as a debounce technique
-  EVERY_N_MILLISECONDS ( 50 )
-  {
-    newStatus = getButtonStatus();
-  }
-
+  EVERY_N_MILLISECONDS ( 50 ) newStatus = getButtonStatus();
+  
   if (newStatus != buttonStatus)
   {
     buttonStatus = newStatus;
@@ -441,9 +604,7 @@ void loop()
 
   gPatterns[gCurrentPatternNumber]();
 
-
   BRIGHTNESS = pos;
-
 }
 
 #define ARRAY_SIZE(A) (sizeof(A) / sizeof((A)[0]))
@@ -451,6 +612,8 @@ void loop()
 void toggleRoutine() {
   Serial.println("toggling routine");
   gCurrentPatternNumber = (gCurrentPatternNumber + 1) % ARRAY_SIZE( gPatterns);
+  fillSolid();
+  delay(500);
 }
 
 // examples of fill and gradients
@@ -518,29 +681,9 @@ void fillGradient2()
     leds[i].fadeLightBy( 255 - BRIGHTNESS );
   }
   FastLED.show();
-
-  //  fill_solid(leds+5,5, CRGB::Green);
-  //  fill_solid(leds+10,5, hsvval);
-
-  // fill_gradient section
-  //  fill_gradient_RGB(leds, startpos, 0x000011, endpos, 0x110000);   // You can mix and match long values and CRGB values. Remember, endpos goes up to NUM_LEDS-1
-  //  fill_gradient_RGB(leds, NUM_LEDS, CRGB(50,0,200), CRGB(80,200,240));  // up to 4 CRGB (or long) values
-
-  //FORWARD_HUES, BACKWARD_HUES, SHORTEST_HUES, LONGEST_HUES
-  //  fill_gradient(leds, startpos, CHSV(50, 255,255) , endpos, CHSV(150,255,255), SHORTEST_HUES);
-  //  fill_gradient(leds, NUM_LEDS, CHSV(50, 255,255), CHSV(100,255,255), LONGEST_HUES);    // up to 4 CHSV values
-
-  // fill_rainbow section
-  //  fill_rainbow(leds, NUM_LEDS, thishue, deltahue);            // Use FastLED's fill_rainbow routine.
-  //  fill_solid(leds,NUM_LEDS,0);                                // Clear the strip for. . .
-  //  fill_rainbow(leds+1, NUM_LEDS-2, thishue, deltahue);        // One pixel border at each end.
-
 }
 
-
-//CRGBPalette16 currentPalette( CRGB::Black);
 CRGBPalette16 currentPalette( Ron_Sky_gp );
-
 CRGBPalette16 targetPalette( Ron_Sky_gp );
 
 // max number of changes to the palette during a blend operation
@@ -550,22 +693,61 @@ uint8_t maxChanges = 5;
 void warmPalette1()
 {
   EVERY_N_MILLISECONDS(1000) {
-    Serial.println("warm palette");
+    Serial.println("warmPalette1");
   };
   EVERY_N_MILLISECONDS(200) {
-    ChangePalettePeriodically();
+    ChangePaletteWarm1();
   };
 
   // rotation speed. lower millis to go faster
   EVERY_N_MILLISECONDS ( 32 ) startIndex++;
 
   // how often to blend and write to the LEDS
-  EVERY_N_MILLISECONDS( 16 ) {
+  EVERY_N_MILLISECONDS( 8) {
     nblendPaletteTowardPalette( currentPalette, targetPalette, maxChanges );
     FillLEDsFromPaletteColors( startIndex);
     FastLED.show();
   };
+}
 
+void coolPalette1()
+{
+  EVERY_N_MILLISECONDS(1000) {
+    Serial.println("coolPalette1");
+  };
+  EVERY_N_MILLISECONDS(200) {
+    ChangePaletteCool1();
+  };
+
+  // rotation speed. lower millis to go faster
+  EVERY_N_MILLISECONDS ( 32 ) startIndex++;
+
+  // how often to blend and write to the LEDS
+  EVERY_N_MILLISECONDS( 8 ) {
+    nblendPaletteTowardPalette( currentPalette, targetPalette, maxChanges );
+    FillLEDsFromPaletteColors( startIndex);
+    FastLED.show();
+  };
+}
+
+void coolPalette2()
+{
+  EVERY_N_MILLISECONDS(1000) {
+    Serial.println("coolPalette2");
+  };
+  EVERY_N_MILLISECONDS(200) {
+    ChangePaletteCool2();
+  };
+
+  // rotation speed. lower millis to go faster
+  EVERY_N_MILLISECONDS ( 32 ) startIndex++;
+
+  // how often to blend and write to the LEDS
+  EVERY_N_MILLISECONDS( 8 ) {
+    nblendPaletteTowardPalette( currentPalette, targetPalette, maxChanges );
+    FillLEDsFromPaletteColors( startIndex);
+    FastLED.show();
+  };
 }
 
 uint8_t gHue = 110; // rotating "base color" used by many of the patterns
@@ -664,16 +846,16 @@ void FillLEDsFromPaletteColors( uint8_t colorIndex)
 {
   for ( int i = 0; i < NUM_LEDS; i++)
   {
-    //    leds[i] = ColorFromPalette( currentPalette, colorIndex + sin8(i*16), BRIGHTNESS);
+      //leds[i] = ColorFromPalette( currentPalette, colorIndex + sin8(i*16), 255);
     placeOnColorPalette = colorIndex + sin8( round((i * (256 / NUM_LEDS)) / 4 )); // <-- change divisor here for how much we spread out the spectrum on the leds
-    leds[i] = ColorFromPalette( currentPalette, placeOnColorPalette, 255); //full bright then dim
+    leds[i] = ColorFromPalette( currentPalette, placeOnColorPalette, 255); //full bright then dim with scaling
     // Dim a color by X/256ths
-    // using "video" scaling, meaning: avoids fading a single led (R,G,B) to 0 which would look wrong a low light levels
+    // using "video" scaling, meaning: avoids fading a single led (R,G,B) to 0 which would look wrong 
     leds[i].fadeLightBy( 255 - BRIGHTNESS );
   }
 }
 
-void ChangePalettePeriodically()
+void ChangePaletteWarm1()
 {
   uint8_t secondHand = (millis() / 1000) % 60;
   static uint8_t lastSecond = 99;
@@ -686,7 +868,7 @@ void ChangePalettePeriodically()
       //     targetPalette = sky_31_gp;
       Serial.println("Ron_Sky_gp");
     }
-    if ( secondHand == 13)  {
+    if ( secondHand == 10)  {
       //      targetPalette = Ron_Vent_gp;
       targetPalette = sky_04_gp;
 
@@ -697,7 +879,7 @@ void ChangePalettePeriodically()
       targetPalette = sky_31_gp;
       Serial.println("sky_31_gp");
     }
-    if ( secondHand == 33)  {
+    if ( secondHand == 30)  {
       targetPalette = Ron_Sky_gp;
       Serial.println("Ron_Sky_gp");
     }
@@ -705,9 +887,78 @@ void ChangePalettePeriodically()
       targetPalette = sky_04_gp;
       Serial.println("sky_04_gp");
     }
-    if ( secondHand == 53)  {
+    if ( secondHand == 50)  {
       targetPalette = sky_31_gp;
       Serial.println("sky_31_gp");
+    }
+  }
+}
+
+void ChangePaletteCool1()
+{
+  uint8_t secondHand = (millis() / 1000) % 60;
+  static uint8_t lastSecond = 99;
+
+  if ( lastSecond != secondHand) {
+    lastSecond = secondHand;
+    if ( secondHand ==  0)  {
+      targetPalette = Warm_summer_day_gp;
+      Serial.println("Warm_summer_day_gp");
+    }
+    if ( secondHand == 10)  {
+      //      targetPalette = Ron_Vent_gp;
+      targetPalette = All_kinds_of_green_gp;
+      Serial.println("All_kinds_of_green_gp");
+    }
+    if ( secondHand == 20)  {
+      targetPalette = sky_21_gp;
+      Serial.println("sky_21_gp");
+    }
+    if ( secondHand == 30)  {
+      targetPalette = springangel_gp;
+      Serial.println("springangel_gp");
+    }
+    if ( secondHand == 40)  {
+      targetPalette = Adrift_in_Dreams_gp;
+      Serial.println("Adrift_in_Dreams_gp");
+    }
+    if ( secondHand == 50)  {
+      targetPalette = springbird_gp;
+      Serial.println("springbird_gp");
+    }
+  }
+}
+
+void ChangePaletteCool2()
+{
+  uint8_t secondHand = (millis() / 1000) % 60;
+  static uint8_t lastSecond = 99;
+
+  if ( lastSecond != secondHand) {
+    lastSecond = secondHand;
+    if ( secondHand ==  0)  {
+      targetPalette = spellbound_gp;
+      Serial.println("spellbound_gp");
+    }
+    if ( secondHand == 10)  {
+      targetPalette = trove_gp;
+      Serial.println("trove_gp");
+    }
+    if ( secondHand == 20)  {
+      targetPalette = sunlitwave_gp;
+      Serial.println("sunlitwave_gp");
+    }
+    if ( secondHand == 30)  {
+      targetPalette = hangonfatboy_gp;
+      Serial.println("hangonfatboy_gp");
+    }
+    if ( secondHand == 40)  {
+      targetPalette = Everglowing_Wonder_gp;
+      Serial.println("Everglowing_Wonder_gp");
+    }
+    if ( secondHand == 50)  {
+      targetPalette = mellon_ball_surprise_gp;
+      Serial.println("mellon_ball_surprise_gp");
     }
   }
 }
